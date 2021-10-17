2972094ac84c6ec6c7238b1c7766e874788a893b
Imran Ahmed <imran.ahmed@qbridgeplatform.com>
2021-10-17 10:37:13 +0100

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'addbd62a4963ceafc71ba30abcf0ca71'),
+    },
+  },
+});
